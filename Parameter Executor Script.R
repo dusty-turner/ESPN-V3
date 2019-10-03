3e6d849a17c071816a494d6a736e53326302d5cb
dusty-turner <dusty.s.turner@gmail.com>
2019-10-02 23:06:39 -0400

    updates from work pushed

--- Parameter Executor Script.R
+++ Parameter Executor Script.R
@@ -241,7 +241,9 @@ WeeklyEstimates$Team  %>% unique() %>% na.omit() -> teamlist
 currentweek = 4
 for (i in 1:10) {
   rmarkdown::render("Fantasy Football Team Report.Rmd",params=list(team=teamlist[i],week = currentweek))
-  file.rename(from="Fantasy-Football-Team-Report.html", to =paste(teamlist[i],"Update.html"))
-  file.copy(from=paste0(getwd(),"/",teamlist[i], " Update.html"), to = paste0(getwd(),"/FF Update Reports/",teamlist[i]," Update.html"), overwrite = TRUE)
-  file.remove(paste0(getwd(),"/",teamlist[i]," Update.html"))
+  file.rename(from="Fantasy-Football-Team-Report.html", to =paste0(teamlist[i],"_Update_week", currentweek, ".html"))
+  file.copy(from=paste0(getwd(),"/",teamlist[i], "_Update_week",currentweek,".html"), to = paste0(getwd(),"/FF Update Reports/",teamlist[i]," Update_week", currentweek, ".html"), overwrite = TRUE)
+  file.remove(paste0(getwd(),"/",teamlist[i],"_Update_week",currentweek,".html"))
 }
+
+file.rename(from="Fantasy-Football-League-Report.html", to =paste0("Fantasy-Football-League-Report_Week_", currentweek, ".html"))
